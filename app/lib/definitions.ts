eedd19ba3e045482f6c32795a3d10c7f2ca464d0
tgomoretti <tgomoretti@gmail.com>
2024-02-02 09:58:43 -0300

    feat:mutating-data

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -79,10 +79,3 @@ export type CustomerField = {
   id: string;
   name: string;
 };
-
-export type InvoiceForm = {
-  id: string;
-  customer_id: string;
-  amount: number;
-  status: 'pending' | 'paid';
-};
